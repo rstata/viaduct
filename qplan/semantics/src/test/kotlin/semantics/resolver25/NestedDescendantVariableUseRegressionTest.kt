219d572cd81675d66f627d1d35b37c7efb9efd83
Raymie Stata <raymie.stata@airbnb.com>
2026-08-11 22:28:51 +0000

    New test cases

---
+++ qplan/semantics/src/test/kotlin/semantics/resolver25/NestedDescendantVariableUseRegressionTest.kt
@@ -0,0 +1,247 @@
+package semantics.resolver25
+
+import model.EngineResult
+import model.Schema
+import model.SelectionForest
+import model.Value
+import model.emptyFragmentOf
+import model.fragmentFrom
+import model.instantiateBindings
+import model.merge
+import model.objectOf
+import model.testing.TestWorld
+import model.testing.fieldResolverOf
+import model.testing.fromObjectField
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NestedDescendantVariableUseRegressionTest {
+    @Test
+    fun `descendant owner contributes nested demand through an existing passive object`() {
+        val resultFragment =
+            """
+            fragment Result on Item {
+              source
+              holder {
+                consume(value: ${'$'}value)
+              }
+            }
+            """.trimIndent()
+        val testWorld =
+            TestWorld.fromSDL(
+                schemaSDL =
+                    """
+                    type Holder {
+                      consume(value: Int!): Int!
+                    }
+
+                    type Item {
+                      source: Int!
+                      holder: Holder!
+                      result: Int!
+                    }
+
+                    type Query {
+                      item: Item!
+                    }
+                    """.trimIndent(),
+                fieldResolvers = { schema ->
+                    val holderKey =
+                        Value.GroundKey.of(
+                            schema.objectField("Item", "holder"),
+                            emptyMap(),
+                        )
+                    val consume = schema.objectField("Holder", "consume")
+                    mapOf(
+                        schema.objectField("Query", "item") to
+                            fieldResolverOf(schema.emptyFragmentOf("Query")) { _, _ ->
+                                schema.objectOf("Item") {
+                                    "source" setTo 7
+                                    "holder" setTo schema.objectOf("Holder")
+                                }
+                            },
+                        schema.objectField("Item", "result") to
+                            fieldResolverOf(schema.fragmentFrom(resultFragment)) { input, _ ->
+                                val holder =
+                                    input.fieldValues.getValue(holderKey) as Value.Object
+                                holder.fieldValues.getValue(
+                                    Value.GroundKey.of(
+                                        consume,
+                                        mapOf("value" to 7),
+                                    ),
+                                )
+                            },
+                        consume to
+                            fieldResolverOf(schema.emptyFragmentOf("Holder")) { _, arguments ->
+                                arguments.fieldValues.getValue("value") as Value.Int
+                            },
+                    )
+                },
+                variableProviders = { schema ->
+                    val result = schema.objectField("Item", "result")
+                    mapOf(
+                        Value.Variable.of(result, "value") to
+                            schema.fromObjectField(resultFragment, listOf("source")),
+                    )
+                },
+            )
+        val world = testWorld.assumptions
+        val itemKey =
+            Value.GroundKey.of(
+                world.schema.objectField("Query", "item"),
+                emptyMap(),
+            )
+        val resultKey =
+            Value.GroundKey.of(
+                world.schema.objectField("Item", "result"),
+                emptyMap(),
+            )
+
+        val resolved =
+            context(world) {
+                world.objectOf("Query").resolve(
+                    world.fragmentFrom(
+                        "fragment ignored on Query { item { result } }",
+                    ).subselections,
+                )
+            }
+        val item = resolved.getValue(itemKey).get() as EngineResult.Object
+
+        assertEquals(Value.Int.of(7), item.getValue(resultKey).get())
+    }
+
+    @Test
+    fun `late descendant demand merges into an existing exact resolver instance`() {
+        var consumeApplications = 0
+        var consumeDemand: SelectionForest? = null
+        val resultFragment =
+            """
+            fragment Result on Item {
+              source
+              holder {
+                consume(value: ${'$'}value) {
+                  two
+                }
+              }
+            }
+            """.trimIndent()
+        val testWorld =
+            TestWorld.fromSDL(
+                schemaSDL =
+                    """
+                    type Payload {
+                      one: Int!
+                      two: Int!
+                    }
+
+                    type Holder {
+                      consume(value: Int!): Payload!
+                    }
+
+                    type Item {
+                      source: Int!
+                      holder: Holder!
+                      result: Int!
+                    }
+
+                    type Query {
+                      item: Item!
+                    }
+                    """.trimIndent(),
+                fieldResolvers = { schema ->
+                    val holderKey =
+                        Value.GroundKey.of(
+                            schema.objectField("Item", "holder"),
+                            emptyMap(),
+                        )
+                    val consume = schema.objectField("Holder", "consume")
+                    val consumeKey = Value.GroundKey.of(consume, mapOf("value" to 7))
+                    val twoKey =
+                        Value.GroundKey.of(
+                            schema.objectField("Payload", "two"),
+                            emptyMap(),
+                        )
+                    mapOf(
+                        schema.objectField("Query", "item") to
+                            fieldResolverOf(schema.emptyFragmentOf("Query")) { _, _ ->
+                                schema.objectOf("Item") {
+                                    "source" setTo 7
+                                    "holder" setTo schema.objectOf("Holder")
+                                }
+                            },
+                        schema.objectField("Item", "result") to
+                            fieldResolverOf(schema.fragmentFrom(resultFragment)) { input, _ ->
+                                val holder =
+                                    input.fieldValues.getValue(holderKey) as Value.Object
+                                val payload =
+                                    holder.fieldValues.getValue(consumeKey) as Value.Object
+                                payload.fieldValues.getValue(twoKey)
+                            },
+                        consume to
+                            fieldResolverOf(schema.emptyFragmentOf("Holder")) { _, _ ->
+                                schema.objectOf("Payload") {
+                                    "one" setTo 3
+                                    "two" setTo 5
+                                }
+                            }.observeApplications { _, _, demand ->
+                                consumeApplications += 1
+                                consumeDemand = demand
+                            },
+                    )
+                },
+                variableProviders = { schema ->
+                    val result = schema.objectField("Item", "result")
+                    mapOf(
+                        Value.Variable.of(result, "value") to
+                            schema.fromObjectField(resultFragment, listOf("source")),
+                    )
+                },
+            )
+        val world = testWorld.assumptions
+        val itemKey =
+            Value.GroundKey.of(
+                world.schema.objectField("Query", "item"),
+                emptyMap(),
+            )
+        val resultKey =
+            Value.GroundKey.of(
+                world.schema.objectField("Item", "result"),
+                emptyMap(),
+            )
+
+        val resolved =
+            context(world) {
+                world.objectOf("Query").resolve(
+                    world.fragmentFrom(
+                        """
+                        fragment ignored on Query {
+                          item {
+                            holder {
+                              consume(value: 7) {
+                                one
+                              }
+                            }
+                            result
+                          }
+                        }
+                        """.trimIndent(),
+                    ).subselections,
+                )
+            }
+        val item = resolved.getValue(itemKey).get() as EngineResult.Object
+        val payloadType = world.schema.type("Payload") as Schema.ObjectType
+
+        assertEquals(Value.Int.of(5), item.getValue(resultKey).get())
+        assertEquals(1, consumeApplications)
+        assertEquals(
+            setOf("one", "two"),
+            context(world) {
+                requireNotNull(consumeDemand)
+                    .merge(payloadType)
+                    .instantiateBindings()
+                    .groundKeys()
+                    .mapTo(linkedSetOf()) { key -> key.field.fieldName }
+            },
+        )
+    }
+}

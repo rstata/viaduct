679d6e627d96dce732b0e192d96962324c3bca32
Raymie Stata <raymie.stata@airbnb.com>
2026-08-30 22:28:25 +0000

    qplan: support query fragments in resolver07-08

--- qplan/semantics/src/test/kotlin/semantics/resolver07/ResolverContractTest.kt
+++ qplan/semantics/src/test/kotlin/semantics/resolver07/ResolverContractTest.kt
@@ -16,6 +16,7 @@ import semantics.contract.EmptyObjectFragmentResolverContract
 import semantics.contract.NodeResolverContract
 import semantics.contract.ObjectFragmentFromArgumentResolverContract
 import semantics.contract.ObjectFragmentResolverContract
+import semantics.contract.QueryFragmentResolverContract
 import semantics.contract.ResolverTaskObservation
 import semantics.contract.SometimesPassiveObjectFragmentResolverContract
 import semantics.contract.SometimesPassiveResolverContract
@@ -26,6 +27,7 @@ class ResolverContractTest :
     NodeResolverContract,
     ObjectFragmentResolverContract,
     ObjectFragmentFromArgumentResolverContract,
+    QueryFragmentResolverContract,
     SometimesPassiveResolverContract,
     SometimesPassiveObjectFragmentResolverContract,
     CompleteResolverOutputPolicyContract,
